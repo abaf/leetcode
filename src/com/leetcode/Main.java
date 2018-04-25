7133deff58c4b1fe10355c43c48097bb1b806923
liujian <liujian@cfpamf.org.cn>
2018-04-25 17:35:05 +0800

    RSA公钥私钥测试

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,8 +1,20 @@
 package com.leetcode;
 
+import com.leetcode.crypt.RSAsecurity;
+
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Map<String, String> keyPairs = RSAsecurity.getKey(1024);
+
+        String rawContent = "测试加密，帐号xxx密码xxx";
+        String  enText = RSAsecurity.enByPriKey(rawContent, keyPairs.get("privateKey"));
+        System.out.println(enText);
+        System.out.println("===================================");
+        String deText = RSAsecurity.deByPubKey(enText, keyPairs.get("publicKey"));
+        System.out.println(deText);
     }
 }
